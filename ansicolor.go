4e3d5f877ff918473b2a4e463226074c690fd9f0
Mitsuhiro Koga <shiena.jp@gmail.com>
2014-05-16 02:45:06 +0900

    Add godoc

--- ansicolor.go
+++ ansicolor.go
@@ -1,7 +1,13 @@
+// Package ansicolor provides color console in Windows as ANSICON.
 package ansicolor
 
 import "io"
 
+// NewAnsiColorWriter creates and initializes a new ansiColorWriter
+// using io.Writer w as its initial contents.
+// In the console of Windows, which change the foreground and background
+// colors of the text by the escape sequence.
+// In the console of other systems, which writes to w all text.
 func NewAnsiColorWriter(w io.Writer) *ansiColorWriter {
 	return &ansiColorWriter{w: w}
 }
