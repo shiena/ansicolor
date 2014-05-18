ac0f302ed715ac8a8da18a0ae46f471aa66106a4
Mitsuhiro Koga <shiena.jp@gmail.com>
2014-05-18 16:06:23 +0900

    Add LICENSE comment

--- ansicolor.go
+++ ansicolor.go
@@ -1,3 +1,7 @@
+// Copyright 2014 shiena Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 // Package ansicolor provides color console in Windows as ANSICON.
 package ansicolor
 
